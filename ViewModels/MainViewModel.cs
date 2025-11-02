8567af5b2dd5cd809c7233f34d70dc1d9fbfd2b1
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2025-11-02 18:10:59 +0900

    [refact]MVVMモデルにディレクティブ構造を変更し、デフォルトタイトルを'AresLitho'に変更

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace AresLitho.ViewModels
+{
+
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            PropertyChanged = delegate { }; // Initialize the event to avoid null issues
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
