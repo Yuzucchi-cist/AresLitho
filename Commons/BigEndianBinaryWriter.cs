7851ed333da28113310d0d6b26aac8f9eaf324aa
Yuzucchi-cist <yuzucchi.cist@gmail.com>
2025-11-05 08:00:03 +0900

    [feat]標準のBinaryReaderおよびBinaryWriterがリトルエンディアンにしか対応していないため、ビックエンディアンの実装を追加

---
+++ Commons/BigEndianBinaryWriter.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Text;
+
+namespace AresLitho.Commons
+{
+    public class BigEndianBinaryWriter : BinaryWriter
+    {
+        public BigEndianBinaryWriter(Stream output) : base(output) { }
+        public BigEndianBinaryWriter(Stream output, Encoding encoding) : base(output, encoding) { }
+        public BigEndianBinaryWriter(Stream output, Encoding encoding, bool leaveOpen) : base(output, encoding, leaveOpen) { }
+        public override void Write(short value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(ushort value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(int value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(uint value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(long value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(ulong value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+        public override void Write(float value)
+        {
+            var data = BitConverter.GetBytes(value);
+            Array.Reverse(data);
+            base.Write(data);
+        }
+    }
+}
