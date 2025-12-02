f135aca6b567970477561962fb74540a3d37c918
Yuzucchi-surface <m.yuzucchi.2022@gmail.com>
2025-12-02 19:42:09 +0900

    [feat] ダミーとして空実装のStlのラスタライザの追加

---
+++ AresLitho/Services/Polygon/PolygonRasterizer.cs
@@ -0,0 +1,21 @@
+﻿using AresLitho.Models;
+using SurfaceAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AresLitho.Services.Polygon
+{
+    public static class PolygonRasterizer
+    {
+        public static BinImage LoadAndRasterize(PolygonModel polygon, float z)
+        {
+            // Temporary stub implementation
+            // TODO: Implement polygon rasterization logic here
+            return new BinImage(10, 10);
+        }
+    }
+}
