f5ab86d9bd98f60003966898689635f1a4929b16
Yuzucchi-surface <m.yuzucchi.2022@gmail.com>
2025-12-03 03:14:43 +0900

    [feat] 間欠露光動作に必要なパラメータを PrinterProfile に追加し、表示、編集機能を実装

--- AresLitho/Models/PrinterProfile.cs
+++ AresLitho/Models/PrinterProfile.cs
@@ -14,13 +14,12 @@ namespace AresLitho.Models
     {
         public float XSize { get; set; }
         public float YSize { get; set; }
-        public float ExposureTime { get; set; }
-
+        public ExposurePulse ExposurePulse;
         public PrinterProfile(float xSize, float ySize, float exposureTime)
         {
             XSize = xSize;
             YSize = ySize;
-            ExposureTime = exposureTime;
+            ExposurePulse = new ExposurePulse(exposureTime, exposureTime, 0f);
         }
 
         public static PrinterProfile FromGoo(GooHeaderInfo gooHeaderInfo)
@@ -37,8 +36,53 @@ namespace AresLitho.Models
         {
             goo.Header.XSize = XSize;
             goo.Header.YSize = YSize;
-            goo.Header.BottomExposureTime = ExposureTime;
+            goo.Header.BottomExposureTime = ExposurePulse.ExposureTime;
             return goo;
+        }
+    }
+
+    internal class ExposurePulse
+    {
+        public float ExposureTime { get; private set; }
+
+        public float OnDuration { get; private set; }
+        public float OffDuration { get; private set; }
+        public float DutyRatio { get => OnDuration / (OnDuration + OffDuration); }
+        public int ExposureCount { get => (int)(ExposureTime / OnDuration); }
+
+        public ExposurePulse(float exposureTime, float onDuration, float offDuration)
+        {
+            ExposureTime = exposureTime;
+            OnDuration = onDuration;
+            OffDuration = offDuration;
+        }
+
+        public void SetExposureTime(float exposureTime)
+        {
+            if (ExposureTime < OnDuration)
+                OnDuration = exposureTime;
+            ExposureTime = exposureTime;
+        }
+
+        public void SetOnOff(float on, float off)
+        {
+            if (on < ExposureTime)
+                OnDuration = on;
+            else
+                OnDuration = ExposureTime;
+            OffDuration = off;
+        }
+
+        public void SetDutyRatio(float dutyRatio)
+        {
+            float total = OnDuration + OffDuration;
+            OnDuration = total * DutyRatio;
+            OffDuration = total * (1 - DutyRatio);
+        }
+
+        public void SetExposureCount(int count)
+        {
+            SetOnOff(ExposureTime / count, OffDuration);
         }
     }
 }
--- AresLitho/ViewModels/MainView/PrinterProfileViewModel.cs
+++ AresLitho/ViewModels/MainView/PrinterProfileViewModel.cs
@@ -21,9 +21,60 @@ namespace AresLitho.ViewModels.MainView
                     () => _model.XSize.ToString(), v => _model.XSize = float.Parse(v)),
                 new(nameof(_model.XSize), "YSize",
                     () => _model.YSize.ToString(), v => _model.YSize = float.Parse(v)),
-                new(nameof(_model.ExposureTime), "露光時間",
-                    () => _model.ExposureTime.ToString(), v => _model.ExposureTime = float.Parse(v)),
+                new(nameof(_model.ExposurePulse.ExposureTime), "露光時間",
+                    () => _model.ExposurePulse.ExposureTime.ToString(), SetExposureTime),
+                new(nameof(_model.ExposurePulse.OnDuration), "連続ON時間",
+                    () => _model.ExposurePulse.OnDuration.ToString(), SetOnDuration),
+                new(nameof(_model.ExposurePulse.OffDuration), "連続OFF時間",
+                    () => _model.ExposurePulse.OffDuration.ToString(), SetOffDuration),
+                new(nameof(_model.ExposurePulse.DutyRatio), "デューティ比",
+                    () => _model.ExposurePulse.DutyRatio.ToString(), SetDutyRatio),
+                new(nameof(_model.ExposurePulse.ExposureCount), "露光回数",
+                    () => _model.ExposurePulse.ExposureCount.ToString(), SetExposureCount),
             };
+        }
+
+        private void SetExposureTime(string v)
+        {
+            if (!float.TryParse(v, out float result)) return;
+            _model.ExposurePulse.SetExposureTime(result);
+            NotifyAllPulseItems();
+        }
+        private void SetOnDuration(string v)
+        {
+            if (!float.TryParse(v, out float result)) return;
+            _model.ExposurePulse.SetOnOff(result, _model.ExposurePulse.OffDuration);
+            NotifyAllPulseItems();
+        }
+        private void SetOffDuration(string v)
+        {
+            if (!float.TryParse(v, out float result)) return;
+            _model.ExposurePulse.SetOnOff(_model.ExposurePulse.OnDuration, result);
+            NotifyAllPulseItems();
+        }
+        private void SetDutyRatio(string v)
+        {
+            if (!float.TryParse(v, out float result)) return;
+            _model.ExposurePulse.SetDutyRatio(result);
+            NotifyAllPulseItems();
+        }
+        private void SetExposureCount(string v)
+        {
+            if (!int.TryParse(v, out int result)) return;
+            _model.ExposurePulse.SetExposureCount(result);
+            NotifyAllPulseItems();
+        }
+
+        private void NotifyAllPulseItems()
+        {
+            foreach (var item in Items)
+                if (item.Name
+                    is nameof(_model.ExposurePulse.ExposureTime)
+                    or nameof(_model.ExposurePulse.OnDuration)
+                    or nameof(_model.ExposurePulse.OffDuration)
+                    or nameof(_model.ExposurePulse.DutyRatio)
+                    or nameof(_model.ExposurePulse.ExposureCount))
+                    item.OnPropertyChanged(nameof(item.Value));
         }
     }
 }
