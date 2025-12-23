2f989c3e4acc16b88bd18cea70e906a413845009
Yuzu@ABSL04 <m2250370@photon.chitose.ac.jp>
2025-12-23 12:07:53 +0900

    [test] add resize tests for BinImage

---
+++ AresLithoTest/TestHelpers/BinImageAssert.cs
@@ -0,0 +1,17 @@
+﻿using AresLitho.Models;
+
+namespace AresLithoTest.TestHelpers
+{
+    public static class BinImageAssert
+    {
+        public static void Matches(BinImage img, string[] pattern)
+        {
+            for (int y = 0; y < pattern.Length; y++)
+                for (int x = 0; x < pattern[y].Length; x++)
+                    Assert.AreEqual(
+                        pattern[y][x] == '#',
+                        img[x,y],
+                        $"Mismatch at ({x},{y})");
+        }
+    }
+}
